880a20865464bc965660f3d6e2bef0067798dfa3
neo1218 <neo1218@yeah.net>
2015-10-21 11:25:57 +0800

    test结束后自动清理

--- manage.py
+++ manage.py
@@ -8,32 +8,36 @@
     2.数据库管理
         1>数据库迁移: python manage.py db init; python manage.py db migrate -m "#"
         2>数据库更新: python manage.py db upgrade
-        3>虚拟数据创建：　python manage.py shell
+        3>虚拟数据创建:　python manage.py shell
                         >>>User.generate_fake(100)
                         >>>NewsPost.generate_fake(100)
                         >>>OriginsPost.generate_fake(100)
                         >>>IntersPost.generate_fake(100)
-    3.shell管理
+    3.shell管理(默认导入)
         python manage.py shell
     4.用户管理
         添加用户
         python manage.py adduser user_email username
+        password:
     5.测试
         python manage.py test (--coverage)
 """
 
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, NewsPost, OriginsPost, IntersPost, NewsComment, OriginsComment, IntersComment
+from app.models import User, Role, Permission, NewsPost, OriginsPost, \
+        IntersPost, NewsComment, OriginsComment, IntersComment
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 import sys
 
 
+# 将默认编码改为utf-8, 避免乱码
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
+# 添加测试覆盖率
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
     import coverage
@@ -41,6 +45,7 @@ if os.environ.get('FLASK_COVERAGE'):
     COV.start()
 
 
+# 定义覆盖后再创建app
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -68,11 +73,15 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test(coverage=False):
-    """Run the unit tests.
-       add coverage code report"""
+    """运行单元测试
+       输出测试覆盖率报告"""
     if coverage and not os.environ.get('FLASK_COVERAGE'):
         import sys
         os.environ['FLASK_COVERAGE'] = '1'
+        # os.execvp: 执行$PATH下的可执行文件
+        # sys.executable: /usr/bin/python
+        # sys.argv: ['manage.py', 'shell']
+        # os.execvp('/usr/bin/python',['/usr/bin/python' 'manage.py', 'shell']
         os.execvp(sys.executable, [sys.executable] + sys.argv)
 
     import unittest
